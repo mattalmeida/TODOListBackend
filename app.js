245391126b1d50bb4a5089e281cc223227a774c0
Matt Almeida <pf1matt@gmail.com>
2020-08-23 17:40:49 -0700

    CORS and express

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express();
-
 const mongoose = require('./database/mongoose');
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.json());
 app.listen(3000, () => console.log("Server Connected on port 3000"));
