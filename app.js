293c756a5dba0e61b4fb7a058df834c69a58c971
Matt Almeida <pf1matt@gmail.com>
2020-08-23 17:42:28 -0700

    crud for list and task

--- app.js
+++ app.js
@@ -9,5 +9,77 @@ app.use((req, res, next) => {
     next();
 });
 
+const List = require('./database/models/list');
+const Task = require('./database/models/task');
 app.use(express.json());
+
+app.get('/lists', (req, res) => {
+    List.find({})
+        .then(lists => res.send(lists))
+        .catch((error) => console.log(error));
+});
+
+app.post('/lists', (req, res) => {
+    (new List({ 'title': req.body.title }))
+        .save()
+        .then((list) => res.send(list))
+        .catch((error) => console.log(error));
+})
+
+app.get('/lists/:listId', (req, res) => {
+    List.find({ _id: req.params.listId })
+        .then((list) => res.send(list))
+        .catch((error) => console.log(error));
+})
+
+app.patch('/lists/:listId', (req, res) => {
+    List.findOneAndUpdate({ '_id': req.params.listId }, { $set: req.body })
+        .then((list) => res.send(list))
+        .catch((error) => console.log(error));
+})
+
+app.delete('/lists/:listId', (req, res) => {
+    const deleteTasks = (list) => {
+        Task.deleteMany({_listId: list._id})
+            .then(() => list)
+            .catch((error) => console.log(error));
+    };
+    const list = List.findByIdAndDelete(req.params.listId)
+                     .then((list) => res.send(deleteTasks(list)))
+                     .catch((error) => console.log(error));
+})
+
+/* http://localhost:3000/lists/:listId/tasks/:taskId */
+
+app.get('/lists/:listId/tasks', (req,res) => {
+    Task.find({ _listId: req.params.listId })
+        .then((tasks) => res.send(tasks))
+        .catch((error) => console.log(error));
+})
+
+app.post('/lists/:listId/tasks', (req,res) => {
+    new Task({ 'title': req.body.title, '_listId': req.params.listId })
+        .save()
+        .then((task) => res.send(task))
+        .catch((error) => console.log(error));
+})
+
+app.get('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.find({ _listId: req.params.listId, _id: req.params.taskId })
+        .then((task) => res.send(task))
+        .catch((error) => console.log(error));
+})
+
+app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndUpdate({ _listId: req.params.listId, _id: req.params.taskId }, { $set: req.body })
+        .then((task) => res.send(task))
+        .catch((error) => console.log(error));
+})
+
+app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndDelete({ _listId: req.params.listId, _id: req.params.taskId })
+        .then((task) => res.send(task))
+        .catch((error) => console.log(error));
+})
+
 app.listen(3000, () => console.log("Server Connected on port 3000"));
