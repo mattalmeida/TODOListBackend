3e5d17928bfd7cdc67dac25b45b9e599ca0f54a6
Matt Almeida <pf1matt@gmail.com>
2020-08-23 20:30:56 -0700

    dockerizing service

--- database/mongoose.js
+++ database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://127.0.0.1:27017/taskmanager', { useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify: false })
+mongoose.connect('mongodb://mongo:27017/todo-task-db', { useNewUrlParser: true, useUnifiedTopology: true , useFindAndModify: false })
     .then(() => console.log("Database connected"))
     .catch((error) => console.log(error));
 
